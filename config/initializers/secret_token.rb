140901d7c47765e43845b8af6615b8dec5c3a5ea
PandRaid <dcdunnin@asu.edu>
2013-11-09 16:01:37 -0700

    added prelim styling

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OLM::Application.config.secret_key_base = 'b201a27aff8225c895ee355d256b7d7cd9825894ff4a3ca3f3a043bbc68d357f58cb6f12998c2596ad36f3f4d49b7f6c3621abdaf9554e71b80db26c26d2da33'
