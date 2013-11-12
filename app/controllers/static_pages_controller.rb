b8e8755d30a3a3e6180f4170440335e5f6e5c002
PandRaid <dcdunnin@asu.edu>
2013-11-12 12:00:42 -0700

    added faq

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,9 @@ class StaticPagesController < ApplicationController
   def contact
   end
 
+  def faq
+  end
+  
   def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 OLM::Application.routes.draw do
   get "static_pages/contact"
   get "static_pages/about"
-  
+  get "static_pages/faq"
+
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
